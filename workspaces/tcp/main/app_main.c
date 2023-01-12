128b040d9466e97830c8b83da3dbebec29d344cf
Duc Trung <trung.lyduc18@gmail.com>
2023-01-12 22:39:35 +0700

    remove unnecessary code

--- workspaces/tcp/main/app_main.c
+++ workspaces/tcp/main/app_main.c
@@ -69,7 +69,6 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
         break;
     case MQTT_EVENT_PUBLISHED:
         ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
-        esp_mqtt_client_subscribe(client,"messages/d75b899b-b7bd-4656-8658-07b609fd68a3/status", 0);
         break;
     case MQTT_EVENT_DATA:
         ESP_LOGI(TAG, "MQTT_EVENT_DATA");
